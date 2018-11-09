1a9a7b2ffe819d6bdc02dae06b23fee785c8b16d
xianghugui <arnoldlve@icloud.com>
2018-11-09 17:55:41 +0800

    add consul

---
+++ micro-consul/consul-service/src/main/java/com/micro/services/ConsulServiceApplication.java
@@ -0,0 +1,25 @@
+package com.micro.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@RestController
+public class ConsulServiceApplication {
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsulServiceApplication.class, args);
+  }
+
+}
