1f29a0669e3c6a1749cf7bb3a8a3eac26fdd177f
jianjun <352077600@qq.com>
2018-12-04 18:36:57 +0800

    添加从数据库查询用户判断其权限信息

--- micro-oauth/src/main/java/com/micro/oauth/mapper/AuthorizationMapper.java
+++ micro-oauth/src/main/java/com/micro/oauth/mapper/AuthorizationMapper.java
@@ -9,9 +9,8 @@ public interface AuthorizationMapper {
 
     /**
      * 根据用户名查询当前用户是否存在
-     * 如果存在则查询出用户的角色，拥有的权限等信息
-     * @return  包括用户信息，角色信息，权限信息的json对象
+     * @return
      */
-    JSONObject userInfo();
+    JSONObject userInfo(JSONObject jsonObject);
 
 }
---
+++ micro-oauth/src/main/java/com/micro/oauth/service/AuthorizationService.java
@@ -0,0 +1,20 @@
+package com.micro.oauth.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.micro.oauth.mapper.AuthorizationMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@Service
+public class AuthorizationService {
+    @Autowired
+    private AuthorizationMapper authorizationMapper;
+
+    JSONObject authorization(JSONObject jsonObject){
+
+        return  null;
+    }
+
+
+}
